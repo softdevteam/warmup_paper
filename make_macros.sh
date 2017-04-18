5e29d027e2bd4f3582ff5317a4a829e740c0fa9d
Sarah Mount <sarah.mount@kcl.ac.uk>
2017-04-18 18:50:59 +0100

    Georges comparison as an automated macro.

--- make_macros.sh
+++ make_macros.sh
@@ -5,12 +5,16 @@
 #
 
 EXPERIMENT_REPO=../warmup_experiment/
-BENCHER3_DATA=bencher3_warmup_07.json.bz2
 BENCHER5_DATA=bencher5_warmup_07.json.bz2
 BENCHER6_DATA=bencher6_warmup_07.json.bz2
+BENCHER7_DATA=bencher7_warmup_07.json.bz2
 
 # Generate outlier_summaries.tex
-${EXPERIMENT_REPO}/bin/create_outlier_macros -o outlier_summaries.tex ${BENCHER3_DATA} ${BENCHER5_DATA} ${BENCHER6_DATA}
+${EXPERIMENT_REPO}/bin/create_outlier_macros -o outlier_summaries.tex ${BENCHER5_DATA} ${BENCHER6_DATA} ${BENCHER7_DATA}
 
 # Generate summary_macros.tex
-${EXPERIMENT_REPO}/bin/create_summary_macros -o summary_macros.tex ${BENCHER3_DATA} ${BENCHER5_DATA} ${BENCHER6_DATA}
+${EXPERIMENT_REPO}/bin/create_summary_macros -o summary_macros.tex ${BENCHER5_DATA} ${BENCHER6_DATA} ${BENCHER7_DATA}
+
+# Generate georges_macros.tex and geroges.tex (a table, which we do not use).
+${EXPERIMENT_REPO}/bin/table_georges_comparison -o georges.tex ${BENCHER5_DATA} ${BENCHER6_DATA} ${BENCHER7_DATA}
+rm georges.tex
